4fb0f65945ffe8d0541c9ae6b3a84e9db7c3c972
pradanag65-hue <pradanag65@gmail.com>
2026-09-24 17:04:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='dompet-v5',F=['./','index.html','icon.svg','manifest.json'];
+const C='dompet-v6',F=['./','index.html','icon.svg','manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
