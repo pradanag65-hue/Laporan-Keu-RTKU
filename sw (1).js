c96d4627c9499e54ab6268e1017ef98294c5cee1
pradanag65-hue <pradanag65@gmail.com>
2026-09-24 15:08:11 +0700

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,6 @@
+const C='dompet-v1',F=['./','index.html','icon.svg','manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
